715b3a0eafff717a201f65955309f4717e618467
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2011-10-13 18:49:54 -0600

    reorder email field in registration form

--- lib/Chatty/Form/Register.pm
+++ lib/Chatty/Form/Register.pm
@@ -5,10 +5,10 @@ extends 'HTML::FormHandler::Model::DBIC';
 
 has '+item_class' => (default => 'Account');
 
-has_field 'email' => (type => 'Email', label => 'Email address');
 has_field 'username' => (label => 'User Nickname', required => 1, unique => 1);
 has_field 'password' => (type => 'Password', required => 1);
 has_field 'password_confirm' => (type => 'PasswordConf', required => 1);
+has_field 'email' => (type => 'Email', label => 'Email address');
 has_field 'submit' => (type => 'Submit', value => 'Register');
 
 has '+unique_messages' => (default => sub {
